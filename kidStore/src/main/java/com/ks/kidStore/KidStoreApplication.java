8663812459ed987190be080005b052057f55c078
Bensadia Omar <128587580+Omar-938@users.noreply.github.com>
2023-11-09 15:42:12 +0100

    Add files via upload

---
+++ kidStore/src/main/java/com/ks/kidStore/KidStoreApplication.java
@@ -0,0 +1,17 @@
+package com.ks.kidStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class KidStoreApplication {
+
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidStoreApplication.class, args);
+	}
+
+}
---
+++ kidStore/src/test/java/com/ks/kidStore/KidStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ks.kidStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
